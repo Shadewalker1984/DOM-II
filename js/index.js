b333e5cbdc3e936f91af9f02b0beea265abf8afd
Shadewalker1984 <Shadewalker1984@yahoo.com>
2019-12-06 20:02:36 -0600

    finished a few, having issues on the creative front on figuring out what I can do with the manipulation that doesn't ruin the sight of the page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,52 @@
 // Your code goes here
+
+let navStop = document.querySelector("nav", (event) => {
+    preventDefault();
+})
+
+/////////////////////////////////////////////////////
+
+let navBoom = document.querySelector('.main-navigation');  
+
+navBoom.addEventListener('mouseenter', (event) => {  // 1
+    navBoom.style.transform = "scale(1.3)";
+    navBoom.style.transition = "all 0.3s";
+    
+})
+navBoom.addEventListener('mouseleave', (event) => {   // 2
+    navBoom.style.transform = "scale(1.0)";
+    navBoom.style.transition = "all 0.3s";
+})
+
+////////////////////////////////////////////
+
+let pic = document.querySelector('.img-content');  // 3
+pic.addEventListener('click', (event) => {
+    pic.style.display = "none" ;
+})
+let picOne = document.querySelector('.text-content');  // 3 return
+picOne.addEventListener('click', (event) => {
+    pic.style.display = "inline-block";
+})
+//////////////////////////////////////////
+
+ document.addEventListener('keypress', logKey);    // 4
+function logKey(e) {
+    console.log( `${e.code}`)
+    };
+
+/////////////////////////////////////////
+
+let contCont = document.querySelector('.content-destination');
+
+contCont.addEventListener('click', (event) => {
+    contCont.style.opacity = "0.1";
+})
+    contCont.addEventListener("dblclick", (event) => {  // 5
+        contCont.style.opacity = "1.0";
+    })
+
+document.addEventListener("scroll", (event) => {
+    let container = document.querySelector('.home');
+    container.style.background = "black";
+})
